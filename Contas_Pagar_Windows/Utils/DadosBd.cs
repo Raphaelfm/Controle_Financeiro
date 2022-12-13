683bf48e9919d40995b6244021731788da34e7ff
Raphael Ferreira de Moraes <raphaelfm.2012@gmail.com>
2022-12-13 11:45:48 -0300

    Cria arquivos para configurações de banco

---
+++ Contas_Pagar_Windows/Utils/DadosBd.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contas_Pagar_Windows.Utils
+{
+    public class DadosBd
+    {
+    }
+}
